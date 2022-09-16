2aea59bc71127fa460f56c1b8efdfff2239d36ab
Jude <judetochyokoye@gmail.com>
2022-09-16 20:40:50 +0100

    minor fixes

--- static/home/slider.js
+++ static/home/slider.js
@@ -1,30 +1,31 @@
 
-// Slider Js
-var counter = 1;
-setInterval(function(){
-    document.querySelector(".desktop .slides #radio"+counter).checked = true;
-
-    // controlling desktop auto slide divs
-    const auto = document.querySelectorAll('.nav-auto div');
-    auto.forEach(div =>{
-        div.style.background = '#c4c4c4';
-    })
-    document.querySelector('.nav-auto .auto-btn'+counter).style.background = '#1236f0';
-    counter++;
-    if(counter > 3){
-        counter = 1;
-    }
-}, 5000);
-var mcounter = 1;
-setInterval(function(){
-    document.querySelector(".mobile #m-radio"+counter).checked = true;
-    mcounter++;
-    if(mcounter > 3){
-        mcounter = 1;
-    }
-}, 5000);
 // slider Js end
 document.addEventListener('DOMContentLoaded', function(){
+    // Slider Js
+    var counter = 1;
+    setInterval(function(){
+        document.querySelector(".desktop .slides #radio"+counter).checked = true;
+    
+        // controlling desktop auto slide divs
+        const auto = document.querySelectorAll('.nav-auto div');
+        auto.forEach(div =>{
+            div.style.background = '#c4c4c4';
+        })
+        document.querySelector('.nav-auto .auto-btn'+counter).style.background = '#1236f0';
+        counter++;
+        if(counter > 3){
+            counter = 1;
+        }
+    }, 10000);
+    var mcounter = 1;
+    setInterval(function(){
+        document.querySelector(".mobile #m-radio"+counter).checked = true;
+        mcounter++;
+        if(mcounter > 3){
+            mcounter = 1;
+        }
+    }, 5000);
+
     window.onscroll = function(){
         firstGridAnimation();
         secondGridAnimation();
