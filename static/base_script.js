e8ba5126b683c01584c60730f3e0ddee631f1459
Jude <judetochyokoye@gmail.com>
2022-09-16 16:10:21 +0100

    added loader, still testing tho

--- static/base_script.js
+++ static/base_script.js
@@ -80,6 +80,16 @@ buttons.forEach(button => {
     })
 })
 
-document.addEventListener('DOMContentLoaded', function(){
-
-})
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+          "body").style.visibility = "hidden";
+        document.querySelector(
+          ".spinner").style.visibility = "visible";
+    } else {
+        document.querySelector(
+          ".spinner").style.display = "none";
+        document.querySelector(
+          "body").style.visibility = "visible";
+    }
+};
